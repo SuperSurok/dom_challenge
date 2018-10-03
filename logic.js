c039e10f63c2ee22da9a1beaa543dde7d844b9ff
Sergey <lebedev_81@magora-systems.com>
2018-10-03 19:36:35 +0300

    Third commit

--- logic.js
+++ logic.js
@@ -1,32 +1,22 @@
-window.onload = function () {
+document.onload = (function () {
+    const stepper = document.querySelector('.stepper');
+    const productCount = document.querySelector('.stepper-input');
     let counter = 0;
-    // Handler amount goods input
-    const countItemHandler = (evt) => {
-        let elem = evt.target;
-        let productCount = document.getElementsByTagName('input');
-        if (elem.classList.contains('up')) {
-            counter += 1;
-            productCount.value = counter;
-        }
-        if (elem.classList.contains('down')) {
-            counter -= 1;
-            console.log(productCount.value = counter);
-            if (productCount.value < 0) {
-                productCount.value = 0;
+
+    const addItems = function(evt){
+        var target = evt.target;
+        if(target.className === 'stepper-arrow down') {
+            if (counter > 0) {
+                counter--;
+            } else {
                 return false;
             }
+        } else if(target.className === 'stepper-arrow up') {
+            counter++;
         }
-    };
+    }
 
-    // Switch handler between prices for square meter and package
-    const priceSelectHandler = (evt) => {
-        evt.preventDefault();
-        const priceSelect = document.querySelector('.unit--select');
-        const priceToggle = priceSelect.querySelector('.ng-binding');
-        if (evt.target.classList.contains('ng-binding')) {
-            priceToggle.classList.toggle('unit--active');
-        }
-    };
-    document.addEventListener('click', priceSelectHandler);
-    document.addEventListener('click', countItemHandler)
-};
+
+    document.body.addEventListener('click', addItems);
+
+})();
